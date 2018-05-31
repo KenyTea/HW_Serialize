2885154fe8a44b4d4e1be58c4f7dc5630dec4286
Alex <vassilyevalexey@gmail.com>
2018-05-31 09:14:44 +0600

    не заполняет лист!

---
+++ HW_Serialize/Module/MyClass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Serialize.Module
+{
+    [Serializable]
+
+    public class MyClass
+    {
+        /*имя; фамилия; телефон; год рождения*/
+        string path = @"C:\Users\AVassilyev\Documents\visual studio 2017\Projects\HW_Serialize\MyFile.csv";
+       public Person p = new Person();
+        //List<Person> person;
+
+        public void AddToFile()
+        {            
+            FileInfo file = new FileInfo(path);
+
+            using (FileStream fs = file.Open(FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.Default))
+                {
+                    //sw.WriteLine(p.Name);
+                    //sw.WriteLine(p.Family);
+                    //sw.WriteLine(p.Tel);
+                    //sw.WriteLine(p.Dfb);
+
+                    sw.Write("Ivan");
+                    sw.Write(" ");
+                    sw.Write("Ivanov");
+                    sw.Write(" ");
+                    sw.Write("8777777770");
+                    sw.Write(" ");
+                    sw.WriteLine("1999");
+
+                    Console.WriteLine("The file is created");
+                }
+            }
+        }
+
+        public void ReadFileAndAddToList()
+        {
+            FileInfo file = new FileInfo(path);
+            
+
+            using (FileStream fs = file.Open(FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs, System.Text.Encoding.Default))
+                {
+                    string temp = sr.ReadLine();
+                    var m = temp.Split(' ');
+                    p.Name = m[0];
+                    p.Family = m[1];
+                    p.Tel = m[2];
+                    p.Dfb = m[3];
+                    p.person.Add(p);
+                }
+            }
+        }
+
+        public void Print()
+        {
+            foreach (Person item in p.person)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Family);
+                Console.WriteLine(item.Tel);
+                Console.WriteLine(item.Dfb);
+            }
+        }
+
+        //public static void SoapSerialize()
+        //{
+        //    SoapFormatter formater = new SoapFormatter();
+
+        //    using (FileStream fs = new FileStream("Person.soap", FileMode.OpenOrCreate))
+        //    {
+        //        formater.Serialize(fs, per)
+        //        formater.Serialize(fs, );
+        //        Console.WriteLine("Serial OK!");
+        //    }
+        //}
+    }
+}
---
+++ HW_Serialize/Module/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Serialize.Module
+{
+    [Serializable]
+
+   public class Person
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Tel { get; set; }
+        public string Dfb { get; set; }
+
+        public List<Person> person;
+
+        //public Person()
+        //{
+        //    Name ="Ivan";
+        //    Family = "Ivanov";
+        //    Tel ="+7777777770";
+        //    Dfb = "1999";
+        //}
+
+        //public void Print()
+        //{
+        //    foreach (var item in person)
+        //    {
+        //        Console.WriteLine(item.Name);
+        //        Console.WriteLine(item.Family);
+        //        Console.WriteLine(item.Tel);
+        //        Console.WriteLine(item.Dfb);
+        //    }
+        //}
+    }
+}
